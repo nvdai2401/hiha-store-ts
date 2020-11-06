c58c1ef322fcc4024ea514cd35260ef7d2969e6a
nvdai2401 <nvdai2401@gmail.com>
2020-11-06 08:42:03 +0700

    refactor Main layout using React.FC

--- src/common/definitions/routes.ts
+++ src/common/definitions/routes.ts
@@ -1,7 +1,11 @@
+export interface ILayout {
+  children: JSX.Element | JSX.Element[];
+}
+
 export interface IRoute {
   path: string;
   title: string;
   exact?: boolean;
   component: React.LazyExoticComponent<React.ComponentType<any>>;
-  layout?: any;
+  layout: React.FC<ILayout>;
 }
